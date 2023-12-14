146d75a270bcdb92cc031fde3d7a3ffa687c662f
evermine18 <edgarsanou@gmail.com>
2023-12-14 22:14:16 +0100

    Some category test with Chat rendering

--- extensions/better-gpt/folders/folders.js
+++ extensions/better-gpt/folders/folders.js
@@ -4,7 +4,8 @@ import API from '../requests';
 class Folders {
     constructor() {
         this.chat_list = undefined;
-        this.chats = [];
+        this.improved_chat_list = undefined;
+        this.chats = {};
         this.init();
     }
 
@@ -44,18 +45,41 @@ class Folders {
     saveElementReferences(){
         const chatElements = document.querySelectorAll('li:has(> div a[href*="/c"])');
         chatElements.forEach(chat => {
-            console.log(chat.getAttribute("href"));
+            const chathref = chat.querySelector('div a').getAttribute("href");
+            const chatid = chathref.split("/")[2];
+            this.chats[chatid] = chat;
         });
+        console.log(this.chats);
+    }
+
+    renderChats(){
+        const improved_chat_list = document.getElementById("improved-chat-list").querySelector('div');
+        const elem = document.createElement("div");
+        elem.innerText = "Category Test 1";
+        const ol = document.createElement("ol");
+        Object.keys(this.chats).forEach(chat => {
+            console.log(this.chats[chat]);
+            ol.appendChild(this.chats[chat]);
+        })
+        elem.appendChild(ol);
+        improved_chat_list.appendChild(elem);
     }
 
     async init(){
-        this.chat_list = document.querySelector('div.flex.flex-col.gap-2.pb-2.dark\\:text-gray-100.text-gray-800.text-sm > div > span')
+        this.chat_list = document.querySelector('div.flex.flex-col.gap-2.pb-2.dark\\:text-gray-100.text-gray-800.text-sm > div')
+        this.chat_list.children[0].id = "chat-list";
+        this.chat_list.children[0].style.display = "none";
+        this.improved_chat_list = document.createElement('span');
+        this.improved_chat_list.id = "improved-chat-list";
+        this.improved_chat_list.innerHTML = '<div class="relative mt-5" data-projection-id="7" style="height: auto; opacity: 1;"></div>';
+        this.chat_list.appendChild(this.improved_chat_list);
         //console.log(localStorage.getItem("accessToken"));
         this.saveElementReferences();
-        this.chat_list.innerHTML = "";
-        this.chats = await API.GET('https://chat.openai.com/backend-api/conversations',{Authorization: localStorage.getItem("accessToken")});
+        //this.chat_list.innerHTML = "";
+        //this.chats = await API.GET('https://chat.openai.com/backend-api/conversations',{Authorization: localStorage.getItem("accessToken")});
         console.log(this.chats);
-        this.chat_list.appendChild(await this.createChatListCategory(this.chats.items));
+        this.renderChats();
+        //this.chat_list.appendChild(await this.createChatListCategory(this.chats.items));
         
     }
 }
